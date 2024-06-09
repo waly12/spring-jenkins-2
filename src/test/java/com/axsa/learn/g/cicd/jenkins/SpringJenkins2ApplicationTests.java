be6f20c73737c58ae13acb923585e31574280c2d
Axmad-wali XASSAN KAHIN <ahassanahmed@gmail.com>
2024-06-09 20:41:49 +0200

    second commit

--- src/test/java/com/axsa/learn/g/cicd/jenkins/SpringJenkins2ApplicationTests.java
+++ src/test/java/com/axsa/learn/g/cicd/jenkins/SpringJenkins2ApplicationTests.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringJenkins2ApplicationTests {
 	
