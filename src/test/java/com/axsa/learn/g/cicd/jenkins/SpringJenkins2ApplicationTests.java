2483897bb589a1cf6b52233c8e37e38807a58803
Axmad-wali XASSAN KAHIN <ahassanahmed@gmail.com>
2024-06-09 21:27:49 +0200

    commit 7

--- src/test/java/com/axsa/learn/g/cicd/jenkins/SpringJenkins2ApplicationTests.java
+++ src/test/java/com/axsa/learn/g/cicd/jenkins/SpringJenkins2ApplicationTests.java
@@ -3,9 +3,14 @@ package com.axsa.learn.g.cicd.jenkins;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class SpringJenkins2ApplicationTests {
 	
 	static Logger logger = LoggerFactory.getLogger(SpringJenkins2ApplicationTests.class);
